de0449e0da678bf7ce33a3ce6d7712ca71715b6e
Aidan <aidanthedev@zohomail.com>
2022-05-10 18:22:05 -0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const discord = require('discord.js')
+
+module.exports = newEmbed
+
+/**
+ * @return {Discord.MessageEmbed}
+ * @param {string} title The title for the embed
+ * @param {string} color The color of the embed Default is set to blue(#0000ff)
+ * @param {string} description The description for the embed
+ */
+
+ // Future Update:
+ //@param {string} footer The footer of the embed
+
+function newEmbed(title, color='#0000ff', description) {
+
+      let userEmbed = {
+        embed: {
+          title: title,
+          color: color,
+          description: description,
+        }
+      }
+  return userEmbed
+}
